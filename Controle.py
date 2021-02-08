68cb7d551fe9295c53dd2de93220b87e3c91cc0c
José Samuel Garcia <30785231+samuelgarca@users.noreply.github.com>
2021-02-08 12:01:52 -0300

    Add files via upload

--- Controle.py
+++ Controle.py
@@ -1,6 +1,7 @@
 from PyQt5 import uic,QtWidgets
 import mysql.connector
 from PyQt5.QtWidgets import QMessageBox
+from datetime import date
 import time
 
 data = ""
@@ -10,6 +11,12 @@ banco = mysql.connector.connect(
     user="root",
     passwd="samuelgarcia",
     database="cadastro_estoques"
+)
+banco2 = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="samuelgarcia",
+    database="sensor_temp"
 )
 
 
@@ -118,10 +125,32 @@ def freezer_3():
 
 
 
+
+def excluir_dados():
+    linha = tela_saida.tableWidget.currentRow() +1
+    coluna = tela_saida.tableWidget.currentColumn() +1
+    test4 = tela_saida.tableWidget.cellWidget(linha, coluna)
+    test5 = tela_saida.tableWidget.currentItem()
+    test7 = tela_saida.tableWidget.editItem(test5)
+    test6 = tela_saida.tableWidget.currentIndex()
+    test8 = tela_saida.tableWidget.setCurrentItem(test5)
+    #test4 = tela_saida.tableWidget.setCellWidget(linha,coluna)
+    print(linha)
+    print(coluna)
+    print(test4)
+    #print(test5)
+    #print(test6)
+    #print(test7)
+    #print(test8)
+
+
+
+
+
 def tela_editar():
     tela_saida.show()
     veredit = tela_saida.comboBox.currentText()
-    print(veredit)
+    #print(veredit)
     #tela_saida.pushButton_5.clicked.connect(editar)
     if veredit == "Freezer 1":
         cursor = banco.cursor()
@@ -130,7 +159,7 @@ def tela_editar():
         dados_lidos = cursor.fetchall()
         tela_saida.tableWidget.setRowCount(len(dados_lidos))
         tela_saida.tableWidget.setColumnCount(4)
-        print(dados_lidos)
+        #print(dados_lidos)
         for i in range(0, len(dados_lidos)):
             for j in range(0, 4):
                 tela_saida.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
@@ -142,11 +171,22 @@ def tela_editar():
         dados_lidos = cursor.fetchall()
         tela_saida.tableWidget.setRowCount(len(dados_lidos))
         tela_saida.tableWidget.setColumnCount(4)
-        print(dados_lidos)
+        linha = tela_saida.tableWidget.currentRow() 
+        coluna = tela_saida.tableWidget.currentColumn() 
+        print(str(dados_lidos[linha][coluna]))
+        
         for i in range(0, len(dados_lidos)):
             for j in range(0, 4):
                 tela_saida.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
-
+        
+        linha = tela_saida.tableWidget.currentRow() 
+        coluna = tela_saida.tableWidget.currentColumn() 
+        test4 = tela_saida.tableWidget.cellWidget(linha, coluna)
+        test6 = tela_saida.tableWidget.currentIndex()
+        #print(str(dados_lidos[linha][coluna]))
+        #print(test6)
+    
+    
     if veredit == "Freezer 3":
         cursor = banco.cursor()
         comando_SQL = "SELECT * FROM estoque WHERE local = 'Frezer 3' "
@@ -154,13 +194,24 @@ def tela_editar():
         dados_lidos = cursor.fetchall()
         tela_saida.tableWidget.setRowCount(len(dados_lidos))
         tela_saida.tableWidget.setColumnCount(4)
-        print(dados_lidos)
+        #print(dados_lidos)
         for i in range(0, len(dados_lidos)):
             for j in range(0, 4):
                 tela_saida.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
 
 
-    
+
+def monitor_():
+    tela_monitor.show()
+    cursor = banco2.cursor()
+    comando_SQL = "SELECT wea_temp FROM temp ORDER BY wea_id DESC LIMIT 1"
+    cursor.execute(comando_SQL,dados)
+    dados_lidos = str(cursor.fetchall())
+    print(dados_lidos)
+    tela_monitor.label_3.setText(dados_lidos[2:6])
+
+
+
 
 
     
@@ -176,11 +227,13 @@ tela_freezer_1=uic.loadUi("freezer_1.ui")
 tela_freezer_2=uic.loadUi("freezer_2.ui")
 tela_freezer_3=uic.loadUi("freezer_3.ui")
 tela_saida=uic.loadUi("saida.ui")
+tela_monitor=uic.loadUi("monitor.ui")
 
 ### Verifica qual botão é apertado ###
 menu.pushButton_2.clicked.connect(tela_estoque_)
 menu.pushButton.clicked.connect(tela_estoque)
 menu.pushButton_3.clicked.connect(tela_editar)
+menu.pushButton_4.clicked.connect(monitor_)
 cadastro.pushButton.clicked.connect(funcao_princip)
 cadastro.pushButton_2.clicked.connect(menu_)
 ver_estoque.pushButton_4.clicked.connect(menu_)
@@ -188,6 +241,8 @@ ver_estoque.pushButton.clicked.connect(freezer_1)
 ver_estoque.pushButton_2.clicked.connect(freezer_2)
 ver_estoque.pushButton_3.clicked.connect(freezer_3)
 tela_saida.pushButton_5.clicked.connect(tela_editar)
+tela_saida.pushButton_5.clicked.connect(excluir_dados)
+
 
 
 #estoque.show()
